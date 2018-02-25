386c1ef83c607a745b9b8e0cd20beea901982498
Kumar <aquaraga@gmail.com>
2018-02-26 01:44:05 +0530

    Train perceptron

--- src/perceptron.js
+++ src/perceptron.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const math = require('mathjs');
+
 class Perceptron {
 	constructor(options) {
 		if (!options) {
@@ -7,14 +9,35 @@ class Perceptron {
 		}
 		this.eta = options.eta || 0.01;
 		this.iter = options.iter || 10;
+		this._weights = [];
 	}
 
 	toString() {
 		return `Learning rate: ${this.eta}, Epochs: ${this.iter}`;
 	}
 
+	train(X, y) {
+		const features = X[0].length + 1
+		this._weights = Array.apply(null, Array(features)).map(Number.prototype.valueOf, 0);
+
+		X.forEach((v, i, m) => {
+		  const predicted = this.predict(v);
+		  const update = this.eta * (y[i] - predicted);
+		  const updateWithoutBias = math.dotMultiply(update, v);
+		  const updateWithBias = [update].concat(updateWithoutBias);
+		  this._weights = math.add(this._weights, updateWithBias);
+		});
+
+	}
+
+	predict(x) {
+		const weightsExceptBias = this._weights.slice(1);
+		const netInput = math.prod(x, math.transpose(weightsExceptBias)) + this._weights[0];
+		return netInput >= 0 ? 1: -1;
+	}
+
 	weights() {
-		return [];
+		return this._weights;
 	}
 }
 
--- test/perceptron.test.js
+++ test/perceptron.test.js
@@ -2,6 +2,7 @@
 var Perceptron = require('../src/perceptron');
 
 var assert = require('assert');
+var math = require('mathjs');
 describe('Perceptron', function() {
   describe('constructor', function() {
     it('should initialize with learning rate and epochs', function() {
@@ -24,6 +25,38 @@ describe('Perceptron', function() {
       let perceptron = new Perceptron();
       
       assert.deepEqual([], perceptron.weights());      
-    })
+    });
+
+    it('should give a weight vector of size one more than the number of features', () => {
+      let perceptron = new Perceptron();
+
+      let X = [[1, 2, 3]];
+      let y = [1]
+      perceptron.train(X, y);
+      
+      assert.equal(4, perceptron.weights().length);      
+    });
+
+    it('should update weight vector as per the perceptron learning rule', () => {
+      let perceptron = new Perceptron({eta: 0.5, iter: 1});
+
+      let X = [[1, 2, 3]];
+      let y = [-1]
+      perceptron.train(X, y);
+      
+      assert.deepEqual([-1, -1, -2, -3], perceptron.weights());      
+
+    });
+
+    it('should update weight vector, for each observation', () => {
+      let perceptron = new Perceptron({eta: 0.5, iter: 1});
+
+      let X = [[2, 1, 2], [1, -1, 3]];
+      let y = [1, -1]
+      perceptron.train(X, y);
+      
+      assert.deepEqual([-1, -1, 1, -3], perceptron.weights());      
+
+    });
   });
 });
